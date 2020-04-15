c10e1a8ba945de73f19df55c7821856b40aff069
sciml-stfc <sciml-stfc@stfc.ac.uk>
2020-04-15 08:54:41 +0100

    Build singularity image

--- scripts/build_artifacts.sh
+++ scripts/build_artifacts.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 sudo docker build -t samueljackson/sciml_bench .
 sudo docker push samueljackson/sciml_bench
-singularity build sciml_bench.sif docker://samueljackson/sciml_bench:latest
+sudo singularity build sciml_bench.sif sciml_bench.cfg
+sudo singularity push -U sciml_bench.sif library://sljack/default/sciml-bench
