6f0954b6274199c2e0ef526ce7532397f78b9b10
sciml-stfc <sciml-stfc@stfc.ac.uk>
2020-05-01 16:36:46 +0100

    Add list command
    Also refactor the benchmarks to remove some duplicate code

--- sciml_bench/core/command.py
+++ sciml_bench/core/command.py
@@ -80,6 +80,8 @@ def set_environment_variables(cpu_only=False, use_amp=False, **kwargs):
 
 @click.group()
 @click.pass_context
+@click.option('--data-dir', default='data', help='Data directory location', envvar='SCIML_BENCH_DATA_DIR')
+@click.option('--model-dir', default='sciml-bench-out', type=str, help='Output directory for model results', envvar='SCIML_BENCH_MODEL_DIR')
 @click.option('--tracking-uri', default=None, type=str, help='Tracking URI for MLFlow', envvar='SCIML_BENCH_TRACKING_URI')
 @click.option('--lr-warmup', default=3, type=int, help='Number of epochs over which to scale the learning rate.')
 @click.option('--cpu-only', default=False, is_flag=True, help='Disable GPU execution')
@@ -100,12 +102,19 @@ def cli(ctx, tracking_uri=None, **kwargs):
         print_header()
         set_environment_variables(**kwargs)
 
+@cli.command('list', help='List benchmarks')
+@click.pass_context
+def cmd_list(ctx, **kwargs):
+    click.echo('Available Benchmarks\n')
+
+    for benchmark in BENCHMARKS:
+        path = Path(ctx.obj['data_dir']).joinpath(benchmark.name.replace('-', '_'))
+        downloaded = path.exists()
+        click.echo('{}\t\tDownloaded: {}'.format(benchmark.name, downloaded))
 
 @cli.command(help='Run all benchmarks with default settings')
 @click_config_file.configuration_option(provider=yaml_provider, implicit=False)
 @click.pass_context
-@click.option('--data-dir', default='data', help='Data directory location', envvar='SCIML_BENCH_DATA_DIR')
-@click.option('--model-dir', default='sciml-bench-out', type=str, help='Output directory for model results', envvar='SCIML_BENCH_MODEL_DIR')
 def all(ctx, data_dir, model_dir, **params):
     model_dir = Path(model_dir)
     model_dir.mkdir(parents=True, exist_ok=True)
@@ -116,20 +125,13 @@ def all(ctx, data_dir, model_dir, **params):
         click.echo("Data directory {} does not exist!".format(data_dir))
         sys.exit()
 
-    LOGGER.info("Running DMS Classifier Benchmark")
-    ctx.invoke(dms_classifier, data_dir=data_dir / 'dms_classifier', model_dir=model_dir)
-
-    LOGGER.info("Running EM Denoise Benchmark")
-    ctx.invoke(em_denoise, data_dir=data_dir / 'em_denoise', model_dir=model_dir)
-
-    LOGGER.info("Running SLSTR Cloud Segmentation Benchmark")
-    ctx.invoke(slstr_cloud, data_dir=data_dir / 'slstr_cloud', model_dir=model_dir)
+    for benchmark in BENCHMARKS:
+        LOGGER.info('Running %s benchmark', benchmark.name)
+        ctx.invoke(benchmark, data_dir=data_dir / benchmark.name, model_dir=model_dir)
 
 @cli.command(help='Run the DMS Classifier Benchmark')
 @click_config_file.configuration_option(provider=yaml_provider, implicit=False)
 @click.pass_context
-@click.option('--data-dir', default='data/dms_classifier', envvar='SCIML_BENCH_DATA_DIR')
-@click.option('--model-dir', default='sciml-bench-out', envvar='SCIML_BENCH_MODEL_DIR')
 @click.option('--epochs', default=10, help='Set number of epochs')
 @click.option('--loss', default='binary_crossentropy', help='Set loss function to use')
 @click.option('--batch-size', default=256, help='Set the batch size for training & test')
@@ -137,19 +139,11 @@ def all(ctx, data_dir, model_dir, **params):
 @click.option('--metrics', '-m', default=['accuracy'], multiple=True, help='Set the metrics to output')
 def dms_classifier(ctx, **kwargs):
     import sciml_bench.dms_classifier.main as dms_classifier_mod
-    mlflow.set_experiment('dms_classifier')
-
-    with DistributedMLFlowRun():
-        kwargs.update(ctx.obj)
-        kwargs['model_dir'] = str(Path(kwargs['model_dir']) / 'dms_classifier')
-        kwargs['metrics'] = list(kwargs['metrics'])
-        dms_classifier_mod.main(**kwargs)
+    _run_benchmark(dms_classifier_mod, ctx, **kwargs)
 
 @cli.command(help='Run the Electron Microscopy Denoise Benchmark')
 @click_config_file.configuration_option(provider=yaml_provider, implicit=False)
 @click.pass_context
-@click.option('--data-dir', default='data/em_denoise', envvar='SCIML_BENCH_DATA_DIR')
-@click.option('--model-dir', default='sciml-bench-out', envvar='SCIML_BENCH_MODEL_DIR')
 @click.option('--epochs', default=10, help='Set number of epochs')
 @click.option('--loss', default='mse', help='Set loss function to use')
 @click.option('--batch-size', default=256, help='Set the batch size for training & test')
@@ -157,21 +151,11 @@ def dms_classifier(ctx, **kwargs):
 @click.option('--metrics', '-m', default=[], multiple=True, help='Set the metrics to output')
 def em_denoise(ctx, **kwargs):
     import sciml_bench.em_denoise.main as em_denoise_mod
-
-    mlflow.set_experiment('em_denoise')
-
-    with DistributedMLFlowRun():
-        kwargs.update(ctx.obj)
-        kwargs['model_dir'] = str(Path(kwargs['model_dir']) / 'em_denoise')
-        kwargs['metrics'] = list(kwargs['metrics'])
-        em_denoise_mod.main(**kwargs)
-
+    _run_benchmark(em_denoise_mod, ctx, **kwargs)
 
 @cli.command(help='Run the SLSTR Cloud Segmentation Benchmark')
 @click_config_file.configuration_option(provider=yaml_provider, implicit=False)
 @click.pass_context
-@click.option('--data-dir', default='data/slstr_cloud', envvar='SCIML_BENCH_DATA_DIR')
-@click.option('--model-dir', default='sciml-bench-out', envvar='SCIML_BENCH_MODEL_DIR')
 @click.option('--epochs', default=30, help='Set number of epochs')
 @click.option('--loss', default='binary_crossentropy', help='Set loss function to use')
 @click.option('--batch-size', default=6, help='Set the batch size for training & test')
@@ -179,13 +163,7 @@ def em_denoise(ctx, **kwargs):
 @click.option('--metrics', '-m', default=['accuracy'], multiple=True, help='Set the metrics to output')
 def slstr_cloud(ctx, **kwargs):
     import sciml_bench.slstr_cloud.main as slstr_cloud_mod
-    mlflow.set_experiment('slstr_cloud')
-
-    with DistributedMLFlowRun():
-        kwargs.update(ctx.obj)
-        kwargs['model_dir'] = str(Path(kwargs['model_dir']) / 'slstr_cloud')
-        kwargs['metrics'] = list(kwargs['metrics'])
-        slstr_cloud_mod.main(**kwargs)
+    _run_benchmark(slstr_cloud_mod, ctx, **kwargs)
 
 @cli.command(help='Download benchmark datasets from remote store')
 @click.argument('name', type=click.Choice(['all', 'em_denoise', 'dms_classifier', 'slstr_cloud']))
@@ -194,6 +172,22 @@ def slstr_cloud(ctx, **kwargs):
 def download(*args, **kwargs):
     download_datasets(*args, **kwargs)
 
+def _run_benchmark(module, ctx, **kwargs):
+    benchmark_name = ctx.command.name.replace('-', '_')
+    mlflow.set_experiment(benchmark_name)
+
+    with DistributedMLFlowRun():
+        kwargs.update(ctx.obj)
+        kwargs['data_dir'] = str(Path(kwargs['data_dir']) / benchmark_name)
+        kwargs['model_dir'] = str(Path(kwargs['model_dir']) / benchmark_name)
+        kwargs['metrics'] = list(kwargs['metrics'])
+        module.main(**kwargs)
+
+BENCHMARKS = [
+    dms_classifier,
+    em_denoise,
+    slstr_cloud
+]
 
 if __name__ == "__main__":
     cli(auto_envvar_prefix='SCIML_BENCH')
