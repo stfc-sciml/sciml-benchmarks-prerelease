f4dc2d3176566507df1aef02810eaf6db3c0d055
sciml-stfc <sciml-stfc@stfc.ac.uk>
2020-07-27 17:51:58 +0100

    Include custom object for backwards compatilbility

--- sciml_bench/benchmarks/dms_classifier/model.py
+++ sciml_bench/benchmarks/dms_classifier/model.py
@@ -1,11 +1,12 @@
 import json
 import sciml_bench.mark
 from tensorflow.keras.models import model_from_json
+from tensorflow.keras.layers.experimental.preprocessing import Normalization
 
 
 @sciml_bench.mark.model_function('dms_classifier')
 def resnet_classifier(input_shape, **kwargs):
     with open('models/dms_classifier/model.json') as handle:
         config = json.load(handle)
-    model = model_from_json(config)
+    model = model_from_json(config, custom_objects={'Normalization': Normalization})
     return model
